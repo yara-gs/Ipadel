99f2700c1fb421c2fd3f04eb4ff5155042a20b7d
YaraGomezSaiz <y.gomez.saiz@hotmail.com>
2021-04-29 18:05:59 +0000

    heroku2

--- src/app.py
+++ src/app.py
@@ -24,9 +24,9 @@ jwt = JWTManager(app)
 
 # database condiguration
 if os.environ.get("DATABASE_URL") is not None:
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 else:
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
