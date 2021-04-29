10c9566fd183c2d31f337c7380f93d8134a1d9a3
nicolas-bitbase <nicolas@bitbase.es>
2021-04-29 16:21:59 +0000

    go

--- src/front/js/pages/configure-profile.js
+++ src/front/js/pages/configure-profile.js
@@ -2,6 +2,97 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 
 export default function ConfigureProfile() {
+	const { actions, store } = useContext(Context);
+	let user = actions.getUser();
+
+	const [todos, setTodos] = useState([]);
+	const [newTodo, SetNewTodo] = useState("");
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/todos/", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			}
+		})
+			.then(response => {
+				console.log(response);
+				return response.json();
+			})
+			.then(responseJson => {
+				setTodos(responseJson);
+			});
+	}, []);
+
+	function CreateTodo() {
+		fetch("https://jsonplaceholder.typicode.com/todos/", {
+			method: "POST",
+			body: JSON.stringify({
+				title: newTodo,
+				body: "",
+				userId: 1
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(responseJson => {
+				let newTodos = [...todos];
+				newTodos.push(responseJson);
+				setTodos(newTodos);
+				SetNewTodo(" ");
+			});
+	}
+
+	function updateTodo(newTitle, id) {
+		fetch("https://jsonplaceholder.typicode.com/todos/" + id + "/", {
+			method: "PUT",
+			body: JSON.stringify({
+				id: id,
+				title: newTitle,
+				body: "",
+				userId: 1
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(responseJson => {
+				console.log(responseJson);
+				// let newTodos = [...todos];
+				// newTodos.push(responseJson);
+				// setTodos(newTodos);
+				// SetNewTodo(" ");
+			});
+	}
+
+	function deleteTodo(id) {
+		fetch("https://jsonplaceholder.typicode.com/todos/" + id + "/", {
+			method: "DELETE"
+		}).then(response => {
+			if (response.ok) {
+				let positionToDelete = -1;
+
+				let newTodos = [...todos];
+				for (let i = 0; i < todos.length; i++) {
+					let newTodo = newTodos[i];
+					if (newTodo.id === id) {
+						positionToDelete = i;
+					}
+				}
+				if (positionToDelete > -1) {
+					newTodos.splice(positionToDelete, 1);
+					setTodos(newTodos);
+				} else console.log("error");
+			}
+		});
+	}
 	return (
 		<body>
 			<form action="/action_page.php" className="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin">
@@ -13,7 +104,7 @@ export default function ConfigureProfile() {
 					</div>
 
 					<div className="w3-rest">
-						<input className="w3-input w3-border" name="first" type="text" placeholder="Nombre" />
+						<input className="w3-input w3-border" name="first" type="text" placeholder={user.username} />
 					</div>
 				</div>
 
