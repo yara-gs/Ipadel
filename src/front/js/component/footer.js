c52431fdba797672893818f5d6f6e8bff2096f84
nicolas-bitbase <nicolas@bitbase.es>
2021-05-13 14:37:42 +0000

    listo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
 		<footer className="w3-center w3-black w3-padding-64">
 			<a href="#home" className="w3-button w3-light-grey">
 				<i className="fa fa-arrow-up w3-margin-right" />
-				To the top
+				Vuelta Arriba
 			</a>
 			<div className="w3-xlarge w3-section">
 				<i className="fa fa-facebook-official w3-hover-opacity" />
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,7 +3,7 @@ import { Context } from "../store/appContext";
 import CardGroup from "react-bootstrap/CardGroup";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-
+import { Link } from "react-router-dom";
 import "../../styles/home.scss";
 
 import "w3-css/w3.css";
@@ -21,8 +21,12 @@ export const Home = () => {
 					alt="boat"
 				/>
 				<div className="w3-display-middle w3-padding-large w3-wide w3-center">
-					<h1 className="w3-hide-medium w3-hide-small w3-xxxlarge w3-text-orange">No te quedes sin jugar</h1>
-					<button className="w3-btn w3-round-xlarge w3-center w3-xxlarge">Jugar Ahora </button>
+					<h1 className="w3-hide-medium w3-hide-small w3-xxxlarge w3-text-white">No te quedes sin jugar</h1>
+					<Link to="/sign">
+						<button className="w3-button w3-hover-orange w3-round-xlarge w3-center w3-xxlarge">
+							Jugar Ahora{" "}
+						</button>
+					</Link>
 				</div>
 			</div>
 
@@ -193,19 +197,19 @@ export const Home = () => {
 			<div className="w3-container w3-row w3-center w3-dark-grey w3-padding-64">
 				<div className="w3-quarter">
 					<span className="w3-xxlarge">14+</span>
-					<p>Partners</p>
+					<p>Colaboradores</p>
 				</div>
 				<div className="w3-quarter">
 					<span className="w3-xxlarge">55+</span>
-					<p>Projects Done</p>
+					<p>Pistas</p>
 				</div>
 				<div className="w3-quarter">
-					<span className="w3-xxlarge">89+</span>
-					<p>Happy Clients</p>
+					<span className="w3-xxlarge">100+</span>
+					<p>Jugadores</p>
 				</div>
 				<div className="w3-quarter">
-					<span className="w3-xxlarge">150+</span>
-					<p>Meetings</p>
+					<span className="w3-xxlarge">50+</span>
+					<p>Torneos</p>
 				</div>
 			</div>
 
@@ -296,130 +300,6 @@ export const Home = () => {
 				</div>
 			</div>
 
-			<div className="w3-container w3-light-grey w3-padding-64">
-				<div className="w3-row-padding">
-					<div className="w3-col m6">
-						<h3>Our Skills.</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-							labore et dolore.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-							labore et dolore.
-						</p>
-					</div>
-					<div className="w3-col m6">
-						<p className="w3-wide">
-							<i className="fa fa-camera w3-margin-right" />
-							Photography
-						</p>
-						<div className="w3-grey">
-							<div className="w3-container w3-dark-grey w3-center" style={{ width: "90%" }}>
-								90%
-							</div>
-						</div>
-						<p className="w3-wide">
-							<i className="fa fa-desktop w3-margin-right" />
-							Web Design
-						</p>
-						<div className="w3-grey">
-							<div className="w3-container w3-dark-grey w3-center" style={{ width: "85%" }}>
-								85%
-							</div>
-						</div>
-						<p className="w3-wide">
-							<i className="fa fa-photo w3-margin-right" />
-							Photoshop
-						</p>
-						<div className="w3-grey">
-							<div className="w3-container w3-dark-grey w3-center" style={{ width: "75%" }}>
-								75%
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div className="w3-container w3-center w3-dark-grey" style={{ padding: "128px 16px" }} id="pricing">
-				<h3>PRICING</h3>
-				<p className="w3-large">Choose a pricing plan that fits your needs.</p>
-				<div className="w3-row-padding" style={{ margin_top: "64px" }}>
-					<div className="w3-third w3-section">
-						<ul className="w3-ul w3-white w3-hover-shadow">
-							<li className="w3-black w3-xlarge w3-padding-32">Basic</li>
-							<li className="w3-padding-16">
-								<b>10GB</b> Storage
-							</li>
-							<li className="w3-padding-16">
-								<b>10</b> Emails
-							</li>
-							<li className="w3-padding-16">
-								<b>10</b> Domains
-							</li>
-							<li className="w3-padding-16">
-								<b>Endless</b> Support
-							</li>
-							<li className="w3-padding-16">
-								<h2 className="w3-wide">$ 10</h2>
-								<span className="w3-opacity">per month</span>
-							</li>
-							<li className="w3-light-grey w3-padding-24">
-								<button className="w3-button w3-black w3-padding-large">Sign Up</button>
-							</li>
-						</ul>
-					</div>
-					<div className="w3-third">
-						<ul className="w3-ul w3-white w3-hover-shadow">
-							<li className="w3-red w3-xlarge w3-padding-48">Pro</li>
-							<li className="w3-padding-16">
-								<b>25GB</b> Storage
-							</li>
-							<li className="w3-padding-16">
-								<b>25</b> Emails
-							</li>
-							<li className="w3-padding-16">
-								<b>25</b> Domains
-							</li>
-							<li className="w3-padding-16">
-								<b>Endless</b> Support
-							</li>
-							<li className="w3-padding-16">
-								<h2 className="w3-wide">$ 25</h2>
-								<span className="w3-opacity">per month</span>
-							</li>
-							<li className="w3-light-grey w3-padding-24">
-								<button className="w3-button w3-black w3-padding-large">Sign Up</button>
-							</li>
-						</ul>
-					</div>
-					<div className="w3-third w3-section">
-						<ul className="w3-ul w3-white w3-hover-shadow">
-							<li className="w3-black w3-xlarge w3-padding-32">Premium</li>
-							<li className="w3-padding-16">
-								<b>50GB</b> Storage
-							</li>
-							<li className="w3-padding-16">
-								<b>50</b> Emails
-							</li>
-							<li className="w3-padding-16">
-								<b>50</b> Domains
-							</li>
-							<li className="w3-padding-16">
-								<b>Endless</b> Support
-							</li>
-							<li className="w3-padding-16">
-								<h2 className="w3-wide">$ 50</h2>
-								<span className="w3-opacity">per month</span>
-							</li>
-							<li className="w3-light-grey w3-padding-24">
-								<button className="w3-button w3-black w3-padding-large">Sign Up</button>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-
 			<div className="w3-container w3-light-grey" style={{ padding: "128px 16px" }} id="contact">
 				<h3 className="w3-center">CONTACTO</h3>
 				<p className="w3-center w3-large">Ponte en contacto con nosotros</p>
@@ -464,14 +344,14 @@ export const Home = () => {
 						</p>
 						<p>
 							<button className="w3-button w3-black" type="submit">
-								<i className="fa fa-paper-plane" /> SEND MESSAGE
+								<i className="fa fa-paper-plane" /> Enviar
 							</button>
 						</p>
 					</form>
 
 					<img
-						src="https://www.w3schools.com/w3images/map.jpg"
-						className="w3-image w3-greyscale"
+						src="https://media.istockphoto.com/photos/balls-of-tennis-near-the-net-picture-id1185437535?k=6&m=1185437535&s=612x612&w=0&h=3Xcjud0iePNC5A30VgJYodLcSWeyaHfzGTVb1Nv1pGY="
+						className="w3-image"
 						style={{ width: "100%" }}
 					/>
 				</div>
