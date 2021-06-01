36db28ae03cdc2719f23e0bf2b6a113f23826a10
nicolas-bitbase <nicolas@bitbase.es>
2021-06-01 23:35:17 +0000

    done

--- src/front/js/component/MiRed/miRedComments.js
+++ src/front/js/component/MiRed/miRedComments.js
@@ -7,11 +7,20 @@ import "w3-css/w3.css";
 
 export default function MiRedComments(props) {
 	return (
-		<div className="w3-col d-flex justify-content-between">
-			<div> {props.comment.text}</div>{" "}
-			<div className=" text_xs">
-				{" "}
-				{props.comment.username} / {props.comment.datetime}{" "}
+		<div className="media border p-3 w3-margin-bottom">
+			<img
+				src="https://www.w3schools.com/w3images/avatar2.png"
+				alt="Avatar"
+				className="w3-left w3-circle w3-margin-right"
+				style={{ width: "35px" }}
+			/>
+
+			<div className="media-body w3-display-container">
+				<h4 className="w3-text-blue">{props.comment.username} </h4>
+				<p> {props.comment.text}</p>
+				<small>
+					<i className="w3-display-bottomright">{props.comment.datetime}</i>
+				</small>
 			</div>
 		</div>
 	);
